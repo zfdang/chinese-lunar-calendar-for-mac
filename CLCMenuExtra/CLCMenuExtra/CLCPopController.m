b2a7309e78494bcb7ddb5b84b9fd269a8844fcd0
Zhengfa DANG <dantifer@gmail.com>
2014-10-31 16:48:55 +0800

    enable navigation by keyboard

--- CLCMenuExtra/CLCMenuExtra/CLCPopController.m
+++ CLCMenuExtra/CLCMenuExtra/CLCPopController.m
@@ -37,5 +37,28 @@
     [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:@"http://calendar.zfdang.com"]];
 }
 
+- (void)keyDown:(NSEvent *)theEvent {
+    NSLog(@"%d", [theEvent keyCode]);
+    switch ([theEvent keyCode]) {
+        case 123:  // left arrow, previous year
+            [webView stringByEvaluatingJavaScriptFromString:@"document.getElementsByTagName(\"BUTTON\")[0].click(); "];
+            break;
+        case 124:  // right arrow, next year
+            [webView stringByEvaluatingJavaScriptFromString:@"document.getElementsByTagName(\"BUTTON\")[3].click(); "];
+            break;
+        case 125:  // down arrow, next month
+            [webView stringByEvaluatingJavaScriptFromString:@"document.getElementsByTagName(\"BUTTON\")[2].click(); "];
+            break;
+        case 126:  // up arrow, previous month
+            [webView stringByEvaluatingJavaScriptFromString:@"document.getElementsByTagName(\"BUTTON\")[1].click(); "];
+            break;
+        case 36:  // return, back to today
+            [webView stringByEvaluatingJavaScriptFromString:@"document.getElementsByTagName(\"BUTTON\")[4].click(); "];
+            break;
+
+        default:
+            break;
+    }
+}
 
 @end
